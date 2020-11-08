13fcb4dd6e0983abe35abc0b305419a789356132
kobashou06 <kobashou06@gmail.com>
2020-11-08 18:28:29 +0900

    tortoise.fillにモディファイアを設定。
    画像の拡大縮小設定、色、余白、フレーム、角丸

--- Todo/Todo/CategoryImage.swift
+++ Todo/Todo/CategoryImage.swift
@@ -11,6 +11,14 @@ import SwiftUI
 struct CategoryImage: View {
     var body: some View {
         Image(systemName:"tortoise.fill")
+        .resizable()
+        .scaledToFit()
+        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+        .padding(2.0)
+        .frame(width:30,height:30)
+        .background(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
+        .cornerRadius(6.0)
+         
     }
 }
 
