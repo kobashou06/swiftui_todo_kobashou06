eefe368cca5b6af2b557d3a1b300bd7384d8f9c3
kobashou06 <kobashou06@gmail.com>
2020-11-08 18:19:03 +0900

    tortoise.fillを表示させる。

---
+++ Todo/Todo/CategoryImage.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryImage.swift
+//  Todo
+//
+//  Created by kobashou06 on 2020/11/08.
+//  Copyright © 2020 kobashou06. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryImage: View {
+    var body: some View {
+        Image(systemName:"tortoise.fill")
+    }
+}
+
+struct CategoryImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryImage()
+    }
+}
