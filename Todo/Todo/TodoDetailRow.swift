375a1c182817d31e0fd951d6e025266e8ee481cf
kobashou06 <kobashou06@gmail.com>
2020-11-13 00:03:37 +0900

    ジェスチャー追加
    ①右にドラッグしてタスク完了
    ②左にドラッグしてタスク未完了

--- Todo/Todo/TodoDetailRow.swift
+++ Todo/Todo/TodoDetailRow.swift
@@ -11,10 +11,13 @@ import SwiftUI
 struct TodoDetailRow: View {
     
     @ObservedObject var todo: TodoEntity
+    var hideIcon = false
     
     var body: some View {
         HStack {
-            CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            if !hideIcon{
+                CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            }
             CheckBox(checked: Binding(get:{
                 self.todo.state == TodoEntity.State.done.rawValue
             }, set:{
@@ -27,7 +30,18 @@ struct TodoDetailRow: View {
                 }
                 
             }.foregroundColor(self.todo.state == TodoEntity.State.done.rawValue ? .secondary : .primary)
-        }
+        }.gesture(DragGesture().onChanged({ value in
+            if value.predictedEndTranslation.width  > 200 {
+                if self.todo.state != TodoEntity.State.done.rawValue{
+                    self.todo.state = TodoEntity.State.done.rawValue
+                }
+            }else if value.predictedEndTranslation.width < -200 {
+                if self.todo.state != TodoEntity.State.todo.rawValue{
+                    self.todo.state = TodoEntity.State.todo.rawValue
+                }
+            }
+            
+        }))
     }
 }
 
@@ -39,6 +53,25 @@ struct TodoDetailRow_Previews: PreviewProvider {
         newTodo.task = "将来への人間関係づくり"
         newTodo.state = TodoEntity.State.todo.rawValue
         newTodo.category = 0
-        return TodoDetailRow(todo: newTodo)
+        
+        let newTodo1 = TodoEntity(context: context)
+        newTodo1.task="クレームへの対応"
+        newTodo.category = 1
+        let newTodo2 = TodoEntity(context: context)
+        newTodo2.task = "無意味な接待や付き合い"
+        newTodo2.category = 2
+        let newTodo3 = TodoEntity(context: context)
+        newTodo3.task = "長時間、必要以上の息抜き"
+        newTodo3.category = 3
+        
+        return VStack(alignment: .leading){
+            VStack {
+                TodoDetailRow(todo: newTodo)
+                TodoDetailRow(todo: newTodo, hideIcon: true)
+                TodoDetailRow(todo: newTodo1)
+                TodoDetailRow(todo: newTodo2)
+                TodoDetailRow(todo: newTodo3)
+            }
+        }
     }
 }
